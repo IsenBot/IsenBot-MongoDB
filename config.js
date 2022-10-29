4ed0d4405aaa29442a9d44557c8bd444f0ccb21b
enchantuer <31900604+enchantuer@users.noreply.github.com>
2022-10-29 18:45:34 +0200

    config for Log class

--- config.js
+++ config.js
@@ -1,4 +1,29 @@
+const chalk = require('chalk');
 require('dotenv').config();
 module.exports = {
     token : process.env.TOKEN,
+    // Config for the log on discord and console
+    log : {
+        // If true, log on discord with embed and not plain text
+        defaultEmbed: true,
+        // Color rule for log message in console
+        consoleColors: {
+            url: chalk.blue.underline,
+            date: chalk.gray.bgBlack,
+            header: chalk.bold,
+            type: {
+                log: chalk.bgHex('#5865f2').black,
+                success: chalk.bgHex('#18c406').black,
+                error: chalk.bgHex('#e40613').black,
+                event: chalk.bgHex('#e3bd2c').black,
+            },
+        },
+        // Color rule for log message in discord as embed
+        embedColors: {
+            log: '#5865f2',
+            success: '#1ce406',
+            error: '#e40613',
+            event: '#e3bd2c',
+        },
+    },
 };
