69e5180fd7437531dad1516aa75175875c637588
enchantuer <florentbutery@gmail.com>
2022-11-07 00:24:58 +0100

    Some log

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.js');
+const { GatewayIntentBits } = require('discord.js');
+const IsenBot = require('./core/IsenBot');
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+async function main() {
+    const startLogo =
+        "    ..  ...  .  -.   -...  ---  -    \n\n" +
+        "██╗███████╗███████╗███╗   ██╗         \n" +
+        "██║██╔════╝██╔════╝████╗  ██║         \n" +
+        "██║███████╗█████╗  ██╔██╗ ██║         \n" +
+        "██║╚════██║██╔══╝  ██║╚██╗██║         \n" +
+        "██║███████║███████╗██║ ╚████║         \n" +
+        "╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝         \n" +
+        "                                      \n" +
+        "            ██████╗  ██████╗ ████████╗\n" +
+        "            ██╔══██╗██╔═══██╗╚══██╔══╝\n" +
+        "            ██████╔╝██║   ██║   ██║   \n" +
+        "            ██╔══██╗██║   ██║   ██║   \n" +
+        "            ██████╔╝╚██████╔╝   ██║   \n" +
+        "            ╚═════╝  ╚═════╝    ╚═╝ \n\n" +
+        "    ..  ...  .  -.   -...  ---  -    \n";
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('... Ready!');
-});
-// Login to Discord with your client's token
-console.log('Login ...');
-client.login(token).then();
+    // Create a new client instance
+    const client = await new IsenBot({ intents: [GatewayIntentBits.Guilds] });
+    // Some logs
+    await client.logger.log({textContent: startLogo, isEmbed: false});
+    await client.logger.log({
+        textContent: `The bot is starting ...`,
+        headers: "CommandLoader",
+        type: "event",
+    });
+    // Load the command in the client instance, so we can execute them when someone use them
+    await client.loadCommand();
+
+    // When the client is ready, run this code (only once)
+    client.once('ready', () => {
+        client.createLoggers();
+        console.log('... Ready!');
+    });
+    // Login to Discord with your client's token
+    console.log('Login ...');
+    await client.login(client.config.token);
+}
+
+main().then();
