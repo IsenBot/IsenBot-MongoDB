ef6e82811eee4cce7f6aa4c02107ff35c790f3da
ackimixs <ackimixs@protonmail.com>
2022-12-01 15:48:05 +0100

    quoi ?

--- event/core/messageCreate.js
+++ event/core/messageCreate.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        const { content, channel, member } = message
+        const { content, member } = message
         if (member?.user.bot) return;
         if (content.toLowerCase().endsWith("quoi")) {
-            return channel.send("Feur :ChadLeandro:")
+            return message.reply({content: "Feur :ChadLeandro:"})
         }
     },
 };
