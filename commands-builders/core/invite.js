ff2096b6d1a0c88387e3734440eddad5bd22cfd2
Allan Cueff <allan.cueff@protonmail.com>
2022-12-06 12:14:55 +0100

    Adding command invite for test purposes

---
+++ commands-builders/core/invite.js
@@ -0,0 +1,21 @@
+const path = require('node:path');
+const { SlashCommandBuilder } = require('discord.js');
+const {initLanguage} = require("../../utility/commandBuilder");
+
+let com = initLanguage(new SlashCommandBuilder()
+        .setName("invite")
+        .setDescription("test")
+        .addChannelOption((channel) =>
+            channel
+                .setName("channelinput")
+                .setDescription("test")
+                .setRequired(false)
+        )
+    , "CORE")
+
+console.log(com)
+
+module.exports = {
+    category: path.basename(__dirname),
+    data: com
+};
