d73d1511c2d1c04ec44c579403d51c9ee146ea05
ackimixs <ackimixs@protonmail.com>
2022-12-06 20:23:21 +0100

    reverse command updated

--- commands-exe/core/reverse.js
+++ commands-exe/core/reverse.js
@@ -1,5 +1,21 @@
 module.exports = async function(interaction) {
-    const query = interaction.options.getString("query");
+    const link = interaction.options.getString(await interaction.client.translate("core/reverse:BUILDER:OPTIONS:NAME"), true).split("/").slice(-3);
 
-    interaction.reply({content: query.split('').reverse().join(), ephemeral: true});
+    if (link.length !== 3) {
+        return await interaction.reply({content: await interaction.translate("core/reverse:ERROR:INVALID_LINK"), ephemeral: true});
+    }
+
+    const channel = await interaction.client.channels.cache.get(link[1]);
+
+    if (!channel) {
+        return await interaction.reply({content: await interaction.translate("core/reverse:ERROR:INVALID_CHANNEL"), ephemeral: true});
+    }
+
+    const message = await channel.messages?.fetch(link[2])
+
+    if (!message || !(await message).content) return await interaction.reply({content: await interaction.translate("core/reverse:ERROR:MESSAGE_NOT_FOUND"), ephemeral: true})
+
+    const reply = await message.reply({content: `Reverse : ${message.content.split('').reverse().join('')}`});
+
+    await interaction.reply({content: reply.url, ephemeral: true});
 };
