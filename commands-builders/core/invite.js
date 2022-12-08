50d1a00de85b4f590cdaa91578b4f2bcaea260c6
Allan Cueff <allan.cueff@protonmail.com>
2022-12-08 14:29:04 +0100

    ESLint corrections

--- commands-builders/core/invite.js
+++ commands-builders/core/invite.js
@@ -1,17 +1,17 @@
 const path = require('node:path');
 const { SlashCommandBuilder } = require('discord.js');
-const {initLanguage} = require("../../utility/commandBuilder");
+const { initLanguage } = require('../../utility/commandBuilder');
 
 module.exports = {
     category: path.basename(__dirname),
     data: initLanguage(new SlashCommandBuilder()
-            .setName("invite")
-            .setDescription("test")
-            .addChannelOption((channel) =>
-                channel
-                    .setName("channelinput")
-                    .setDescription("test")
-                    .setRequired(false)
-            )
-        , "CORE")
+        .setName('invite')
+        .setDescription('test')
+        .addChannelOption((channel) =>
+            channel
+                .setName('channelinput')
+                .setDescription('test')
+                .setRequired(false),
+        )
+    , 'CORE'),
 };
--- utility/commandBuilder.js
+++ utility/commandBuilder.js
@@ -1,12 +1,12 @@
-const {client} = require("../index");
+const { client } = require('../index');
 
-function initLanguage(builder, category, path = "") {
+function initLanguage(builder, category, path = '') {
     path = path.trim().replaceAll(':', '');
     path += path.length !== 0 ? ':' : '';
-    category = category.endsWith(':') ? category : category + "/" + builder.name + ":";
-    builder.setNameLocalizations(client.getLocales(category + "BUILDER:" + path + "NAME"));
-    builder.setDescriptionLocalizations(client.getLocales(category + "BUILDER:" + path + "NAME"));
-    if(builder.options) {
+    category = category.endsWith(':') ? category : category + '/' + builder.name + ':';
+    builder.setNameLocalizations(client.getLocales(category + 'BUILDER:' + path + 'NAME'));
+    builder.setDescriptionLocalizations(client.getLocales(category + 'BUILDER:' + path + 'NAME'));
+    if (builder.options) {
         for (const option of builder.options) {
             initLanguage(option, category, option.name);
         }
