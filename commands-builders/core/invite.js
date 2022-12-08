0f2aca96fbb01d68ac5d7ea92919be5e15909137
Allan Cueff <allan.cueff@protonmail.com>
2022-12-08 11:37:09 +0100

    New language system bugs fix

--- commands-builders/core/invite.js
+++ commands-builders/core/invite.js
@@ -2,18 +2,16 @@ const path = require('node:path');
 const { SlashCommandBuilder } = require('discord.js');
 const {initLanguage} = require("../../utility/commandBuilder");
 
-let com = initLanguage(new SlashCommandBuilder()
-        .setName("invite")
-        .setDescription("test")
-        .addChannelOption((channel) =>
-            channel
-                .setName("channelinput")
-                .setDescription("test")
-                .setRequired(false)
-        )
-    , "CORE")
-
 module.exports = {
     category: path.basename(__dirname),
-    data: com
+    data: initLanguage(new SlashCommandBuilder()
+            .setName("invite")
+            .setDescription("test")
+            .addChannelOption((channel) =>
+                channel
+                    .setName("channelinput")
+                    .setDescription("test")
+                    .setRequired(false)
+            )
+        , "CORE")
 };
