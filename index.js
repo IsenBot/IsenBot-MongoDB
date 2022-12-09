2ff201afa35d9079e47242744085bac7e441c520
Allan Cueff <allan.cueff@protonmail.com>
2022-12-09 22:14:45 +0100

    mongoDB connector modif Set only one 'mongodb.connection()' for all usages Cache for 'mongodb.db()'

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ Object.defineProperties(BaseInteraction.prototype, {
             return this.logger?.log(...param);
         },
     },
+    mongodb: {
+        get: async function() {
+            return await this.client.guildDB(this.guildId);
+        },
+    },
 });
 
 const { SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder, SlashCommandChannelOption, SlashCommandMentionableOption, SlashCommandIntegerOption, SlashCommandNumberOption, SlashCommandRoleOption, SlashCommandStringOption, SlashCommandUserOption, SlashCommandBooleanOption } = require('discord.js');
