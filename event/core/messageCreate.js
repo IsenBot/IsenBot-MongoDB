ee59c2abf8fea1a12cb27b64fedb25186ecc14a6
Allan Cueff <allan.cueff@protonmail.com>
2022-12-09 22:09:59 +0100

    ESLint corrections

--- event/core/messageCreate.js
+++ event/core/messageCreate.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        const { content, member } = message
+        const { content, member } = message;
         if (member?.user.bot) return;
-        if (content.toLowerCase().endsWith("quoi")) {
-            return message.reply({content: "Feur :ChadLeandro:"})
+        if (content.toLowerCase().endsWith('quoi')) {
+            return message.reply({ content: 'Feur :ChadLeandro:' });
         }
     },
 };
