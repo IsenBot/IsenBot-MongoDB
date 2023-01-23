8bd3a712e33e542873e5381d2535973b55d7bf95
Allan Cueff <allan.cueff@protonmail.com>
2023-01-23 20:07:03 +0100

    debug log

--- .github/actions/update_version/updateVersion.js
+++ .github/actions/update_version/updateVersion.js
@@ -8,7 +8,8 @@ core.info('Output to the actions build log')
 
 core.notice('This is a message that will also emit an annotation')
 
-core.notice(path.dirname());
+core.notice(__dirname);
+core.notice(path.resolve('./'));
 core.notice(path.resolve('../../../'));
 
 fs.readFile('../../../package.json', (e, data) => {
