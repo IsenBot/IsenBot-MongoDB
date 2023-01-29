d566d5748cb72a26062ac2117e4e906550c50829
ackimixs <ackimixs@protonmail.com>
2023-01-29 15:55:29 +0100

    patch for spotify url

--- commands-exe/music/music/music/play/spotify.js
+++ commands-exe/music/music/music/play/spotify.js
@@ -1,6 +1,10 @@
-const { isUrl } = require('../../../../../utility/Function');
+const { isUrl, checkUserChannel } = require('../../../../../utility/Function');
 
 module.exports = async (interaction) => {
+
+    const check = await checkUserChannel(interaction);
+
+    if (!check) return;
 
     const client = interaction.client;
 
@@ -18,8 +22,8 @@ module.exports = async (interaction) => {
 
         const url = new URL(query);
 
-        if (url.protocol.includes('spotify')) {
-            query = url.pathname.split(':');
+        if (url.protocol.includes('spotify') || url.hostname.includes('spotify')) {
+            query = parse(url);
 
             const data = await client.player.spotifyClient.getById(query[1], query[0]);
 
@@ -41,9 +45,12 @@ module.exports = async (interaction) => {
                 if (data.tracks) {
                     const listTrack = [];
                     await interaction.editReply({ content: await interaction.translate('music/music:exe:play:loading') });
-                    for (const track of data.tracks.items) {
-                        if (track?.name) {
-                            const ytTrack = await client.player.searchYoutubeTrack(`${track.name} ${track.artists[0].name}`);
+                    // eslint-disable-next-line prefer-const
+                    for (let [index, value] of data.tracks.items.entries()) {
+                        await interaction.editReply({ content: await interaction.translate('music/music:exe:play:loading') + ` ${~~(((index + 1) / data.tracks.items.length) * 100)}%` });
+                        if (data.type === 'playlist') value = value?.track;
+                        if (value?.name) {
+                            const ytTrack = await client.player.searchYoutubeTrack(`${value.name} ${value.artists[0].name}`);
                             if (ytTrack) {
                                 ytTrack.type = 'spotify';
                                 listTrack.push(ytTrack);
@@ -79,4 +86,10 @@ module.exports = async (interaction) => {
             queue.play();
         }, 1000);
     }
+};
+
+const parse = (url) => {
+    const path = url.pathname.split('/');
+    if (path.length > 1) return [path[1], path[2]];
+    return url.pathname.split(':');
 };
