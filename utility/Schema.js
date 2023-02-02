2b464444ce54807508a0281cdd09b914f45bdf8b
enchantuer <31900604+enchantuer@users.noreply.github.com>
2023-02-03 00:23:12 +0100

    Modify the schema
    RoleReactionSchema, GuildSchema

--- utility/Schema.js
+++ utility/Schema.js
@@ -1,23 +1,30 @@
 class GuildSchema {
     constructor(fields) {
-        if (!fields?._id) {
-            throw new Error('you must give at least the _id field');
+        if (!fields?.messageId) {
+            throw new Error('you must give at least the messageId field');
         }
-        const { _id, logChannelId } = fields;
-        this._id = _id;
+        const { messageId, logChannelId } = fields;
+        this.messageId = messageId;
         this.logChannelId = logChannelId ?? null;
     }
 }
 
 class RoleReactionSchema {
     constructor(fields) {
-        if (!fields?._id) {
-            throw new Error('you must give at least the _id field');
+        if (!(fields?.messageId && fields?.reaction)) {
+            throw new Error('You must give at least the messageId and reaction field');
         }
-        const { _id, channelId, guildId } = fields;
-        this._id = _id;
+        const { messageId, channelId, guildId, reaction, roles, isMultipleChoice } = fields;
+        // noinspection EqualityComparisonWithCoercionJS
+        if (roles == undefined) {
+            throw new Error('You must provide at least one role');
+        }
+        this.messageId = messageId;
+        this.reaction = reaction ?? null;
         this.channelId = channelId ?? null;
         this.guildId = guildId ?? null;
+        this.isMultipleChoice = !!isMultipleChoice;
+        this.roles = (typeof roles === 'string' || roles instanceof String) ? [roles] : roles;
     }
 }
 
