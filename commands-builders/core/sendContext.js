acbdaffc734eea240fe4dac8c6f2fc48d0f21c5a
Allan Cueff <allan.cueff@protonmail.com>
2023-02-12 23:10:25 +0100

    Added sendContext  => Send a message to a channel by right click on this message

---
+++ commands-builders/core/sendContext.js
@@ -0,0 +1,11 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const { initContextLanguage } = require('../../utility/commandBuilder');
+const path = require('node:path');
+
+module.exports = {
+    category: path.basename(__dirname),
+    data: initContextLanguage(new ContextMenuCommandBuilder()
+        .setName('sendContext')
+        .setType(ApplicationCommandType.Message)
+    , 'CORE'),
+};
--- utility/commandBuilder.js
+++ utility/commandBuilder.js
@@ -15,3 +15,18 @@ function initLanguage(builder, category, path = '') {
 }
 
 exports.initLanguage = initLanguage;
+
+function initContextLanguage(builder, category, path = '') { // for contexts => dont't have description
+    path = path.trim().replaceAll(':', '');
+    path += path.length !== 0 ? ':' : '';
+    category = category.endsWith(':') ? category : category + '/' + builder.name + ':';
+    builder.setNameLocalizations(client.getLocales(category + 'BUILDER:' + path + 'NAME'));
+    if (builder.options) {
+        for (const option of builder.options) {
+            initContextLanguage(option, category, option.name);
+        }
+    }
+    return builder;
+}
+
+exports.initContextLanguage = initContextLanguage;
