20a33aaedfd77b9695c67ea5cbc152e84fd72f7b
Allan Cueff <allan.cueff@protonmail.com>
2023-03-01 13:31:53 +0100

    no-upgrade patch builder error (it was late yesterday)

--- commands-builders/admin/permission.js
+++ commands-builders/admin/permission.js
@@ -7,51 +7,51 @@ module.exports = {
     data: initLanguage(new SlashCommandBuilder()
         .setName('permission')
         .setDescription('permission manager')
-        .addSubcommand(s => {
+        .addSubcommand(s =>
             s.setName('specify')
-            .setDescription('setup permission to use a command for a specified user, role or channel')
-            .addStringOption(o => {
-                o.setName('command')
-                .setDescription('bot command to setup permission')
-                .setRequired(true)
-                .setAutocomplete(true);
-            })
-            .addBooleanOption(o => {
-                o.setName('authorization')
-                .setDescription('permission')
-                .setRequired(true);
-            })
-            .addRoleOption(o => {
-                o.setName('role')
-                .setDescription('role to setup permission for')
-                .setRequired(false);
-            })
-            .addUserOption(o => {
-                o.setName('user')
-                .setDescription('user to setup permission for')
-                .setRequired(false);
-            })
-            .addChannelOption(o => {
-                o.setName('channel')
-                .setDescription('channel in wich permission will be setup')
-                .setRequired(false);
-            });
-        })
-        .addSubcommand(s => {
+                .setDescription('setup permission to use a command for a specified user, role or channel')
+                .addStringOption(o =>
+                    o.setName('command')
+                        .setDescription('bot command to setup permission')
+                        .setRequired(true)
+                        .setAutocomplete(true),
+                )
+                .addBooleanOption(o =>
+                    o.setName('authorization')
+                        .setDescription('permission')
+                        .setRequired(true),
+                )
+                .addRoleOption(o =>
+                    o.setName('role')
+                        .setDescription('role to setup permission for')
+                        .setRequired(false),
+                )
+                .addUserOption(o =>
+                    o.setName('user')
+                        .setDescription('user to setup permission for')
+                        .setRequired(false),
+                )
+                .addChannelOption(o =>
+                    o.setName('channel')
+                        .setDescription('channel in wich permission will be setup')
+                        .setRequired(false),
+                ),
+        )
+        .addSubcommand(s =>
             s.setName('default')
-            .setDescription('setup permission to use a command for all on all channels')
-            .addStringOption(o => {
-                o.setName('command')
-                .setDescription('bot command to setup permission')
-                .setRequired(true)
-                .setAutocomplete(true);
-            })
-            .addBooleanOption(o => {
-                o.setName('authorization')
-                .setDescription('permission')
-                .setRequired(true);
-            });
-        })
+                .setDescription('setup permission to use a command for all on all channels')
+                .addStringOption(o =>
+                    o.setName('command')
+                        .setDescription('bot command to setup permission')
+                        .setRequired(true)
+                        .setAutocomplete(true),
+                )
+                .addBooleanOption(o =>
+                    o.setName('authorization')
+                        .setDescription('permission')
+                        .setRequired(true),
+                ),
+        )
         .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
     , 'admin'),
 };
--- commands-builders/music/music.js
+++ commands-builders/music/music.js
@@ -7,160 +7,160 @@ module.exports = {
     data: initLanguage(new SlashCommandBuilder()
         .setName('music')
         .setDescription('music commands')
-        .addSubcommandGroup(g => {
+        .addSubcommandGroup(g =>
             g.setName('play')
-            .setDescription('play music')
-            .addSubcommand(s => {
-                s.setName('youtube')
-                .setDescription('play music from youtube')
-                .addStringOption(o => {
-                    o.setName('query')
-                    .setDescription('query to search')
-                    .setRequired(true)
-                    .setAutocomplete(true);
-                });
-            })
-            .addSubcommand(s => {
-                s.setName('spotify')
-                .setDescription('play music from spotify')
-                .addStringOption(o => {
-                    o.setName('query')
-                    .setDescription('query to search')
-                    .setRequired(true)
-                    .setAutocomplete(true);
-                });
-            })
-            .addSubcommand(s => {
-                s.setName('twitch')
-                .setDescription('play stream sound from twitch')
-                .addStringOption(o => {
-                    o.setName('query')
-                    .setDescription('query to search')
-                    .setRequired(true)
-                    .setAutocomplete(true);
-                });
-            });
-        })
-        .addSubcommand(s => {
+                .setDescription('play music')
+                .addSubcommand(s =>
+                    s.setName('youtube')
+                        .setDescription('play music from youtube')
+                        .addStringOption(o =>
+                            o.setName('query')
+                                .setDescription('query to search')
+                                .setRequired(true)
+                                .setAutocomplete(true),
+                        ),
+                )
+                .addSubcommand(s =>
+                    s.setName('spotify')
+                        .setDescription('play music from spotify')
+                        .addStringOption(o =>
+                            o.setName('query')
+                                .setDescription('query to search')
+                                .setRequired(true)
+                                .setAutocomplete(true),
+                        ),
+                )
+                .addSubcommand(s =>
+                    s.setName('twitch')
+                        .setDescription('play stream sound from twitch')
+                        .addStringOption(o =>
+                            o.setName('query')
+                                .setDescription('query to search')
+                                .setRequired(true)
+                                .setAutocomplete(true),
+                        ),
+                ),
+        )
+        .addSubcommand(s =>
             s.setName('stop')
-            .setDescription('stop music');
-        })
-        .addSubcommand(s => {
+                .setDescription('stop music'),
+        )
+        .addSubcommand(s =>
             s.setName('skip')
-            .setDescription('skip music');
-        })
-        .addSubcommand(s => {
+                .setDescription('skip music'),
+        )
+        .addSubcommand(s =>
             s.setName('pause')
-            .setDescription('pause music')
-            .addStringOption(o => {
-                o.setName('mode')
-                .setDescription('mode')
-                .setRequired(true)
-                .addChoices(
-                    {
-                        name: 'pause',
-                        value: 'pause',
-                    },
-                    {
-                        name: 'resume',
-                        value: 'resume',
-                    },
-                );
-            });
-        })
-        .addSubcommand(s => {
+                .setDescription('pause music')
+                .addStringOption(o =>
+                    o.setName('mode')
+                        .setDescription('mode')
+                        .setRequired(true)
+                        .addChoices(
+                            {
+                                name: 'pause',
+                                value: 'pause',
+                            },
+                            {
+                                name: 'resume',
+                                value: 'resume',
+                            },
+                        ),
+                ),
+        )
+        .addSubcommand(s =>
             s.setName('volume')
-            .setDescription('change volume')
-            .addIntegerOption(o => {
-                o.setName('volume')
-                .setDescription('volume to set');
-            });
-        })
-        .addSubcommand(s => {
+                .setDescription('change volume')
+                .addIntegerOption(o =>
+                    o.setName('volume')
+                        .setDescription('volume to set'),
+                ),
+        )
+        .addSubcommand(s =>
             s.setName('queue')
-            .setDescription('show queue')
-            .addIntegerOption(o => {
-                o.setName('page')
-                .setDescription('page to show')
-                .setRequired(false);
-            });
-        })
-        .addSubcommand(s => {
+                .setDescription('show queue')
+                .addIntegerOption(o =>
+                    o.setName('page')
+                        .setDescription('page to show')
+                        .setRequired(false),
+                ),
+        )
+        .addSubcommand(s =>
             s.setName('loop')
-            .setDescription('loop mode')
-            .addIntegerOption(o => {
-                o.setName('mode')
-                .setDescription('mode')
-                .setRequired(true)
-                .addChoices(
-                    {
-                        name: 'off',
-                        value: 0,
-                    },
-                    {
-                        name: 'track',
-                        value: 1,
-                    },
-                    {
-                        name: 'queue',
-                        value: 2,
-                    },
-                    {
-                        name: 'random',
-                        value: 3,
-                    },
-                );
-            });
-        })
-        .addSubcommandGroup(s => {
+                .setDescription('loop mode')
+                .addIntegerOption(o =>
+                    o.setName('mode')
+                        .setDescription('mode')
+                        .setRequired(true)
+                        .addChoices(
+                            {
+                                name: 'off',
+                                value: 0,
+                            },
+                            {
+                                name: 'track',
+                                value: 1,
+                            },
+                            {
+                                name: 'queue',
+                                value: 2,
+                            },
+                            {
+                                name: 'random',
+                                value: 3,
+                            },
+                        ),
+                ),
+        )
+        .addSubcommandGroup(s =>
             s.setName('search')
-            .setDescription('search music')
-            .addSubcommand(ss => {
-                ss.setName('twitch')
-                .setDescription('search music from youtube')
-                .addStringOption(o => {
-                    o.setName('query')
-                    .setDescription('query to search');
-                })
-                .addStringOption(o => {
-                    o.setName('language')
-                    .setDescription('language of the user')
-                    .addChoices(
-                        {
-                            name: 'English',
-                            value: 'en',
-                        },
-                        {
-                            name: 'Francais',
-                            value: 'fr',
-                        },
-                        {
-                            name: 'Español',
-                            value: 'es',
-                        },
-                    )
-                    .setRequired(false);
-                })
-                .addBooleanOption(o => {
-                    o.setName('live')
-                    .setDescription('live stream')
-                    .setRequired(false);
-                })
-                .addIntegerOption(o => {
-                    o.setName('limit')
-                    .setDescription('limit of the search')
-                    .setRequired(false);
-                });
-            });
-        })
-        .addSubcommand(s => {
+                .setDescription('search music')
+                .addSubcommand(ss =>
+                    ss.setName('twitch')
+                        .setDescription('search music from youtube')
+                        .addStringOption(o =>
+                            o.setName('query')
+                                .setDescription('query to search'),
+                        )
+                        .addStringOption(o =>
+                            o.setName('language')
+                                .setDescription('language of the user')
+                                .addChoices(
+                                    {
+                                        name: 'English',
+                                        value: 'en',
+                                    },
+                                    {
+                                        name: 'Francais',
+                                        value: 'fr',
+                                    },
+                                    {
+                                        name: 'Español',
+                                        value: 'es',
+                                    },
+                                )
+                                .setRequired(false),
+                        )
+                        .addBooleanOption(o =>
+                            o.setName('live')
+                                .setDescription('live stream')
+                                .setRequired(false),
+                        )
+                        .addIntegerOption(o =>
+                            o.setName('limit')
+                                .setDescription('limit of the search')
+                                .setRequired(false),
+                        ),
+                ),
+        )
+        .addSubcommand(s =>
             s.setName('remove')
-            .setDescription('remove a track')
-            .addIntegerOption(o => {
-                o.setName('index')
-                .setDescription('index of the track to remove')
-                .setRequired(true);
-            });
-        })
+                .setDescription('remove a track')
+                .addIntegerOption(o =>
+                    o.setName('index')
+                        .setDescription('index of the track to remove')
+                        .setRequired(true),
+                ),
+        )
     , 'music'),
 };
